5eca3843d2c0b7a91160ed04c78a5dc61644991e
jinyongkwon <wlsdyd1178@naver.com>
2022-05-13 22:26:43 +0900

    딱 필요한것만 있게끔 수정

--- Springboot-MongoDB-Junit5/src/main/java/site/metacoding/mongocrud/web/NaverApiController.java
+++ Springboot-MongoDB-Junit5/src/main/java/site/metacoding/mongocrud/web/NaverApiController.java
@@ -3,12 +3,9 @@ package site.metacoding.mongocrud.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import site.metacoding.mongocrud.domain.Naver;
 import site.metacoding.mongocrud.domain.NaverRepository;
 
 @RequiredArgsConstructor
@@ -21,8 +18,4 @@ public class NaverApiController {
         return new ResponseEntity<>(naverRepository.findAll(), HttpStatus.OK);
     }
 
-    @PostMapping("/navers")
-    public ResponseEntity<?> save(@RequestBody Naver naver) {
-        return new ResponseEntity<>(naverRepository.save(naver), HttpStatus.CREATED);
-    }
 }
